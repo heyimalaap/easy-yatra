3c034a930e1c5894be9e256b146380d81eab6859
Alaap Surendran <alaapsurendran@gmail.com>
2021-10-29 00:02:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UIComponent from './components/UIComponent';
+
+class App extends React.Component {
+  mapRef = React.createRef();
+
+  state = {
+    map: null,
+    platform: null
+  };
+
+  componentDidMount() {
+    const H = window.H;
+    const platform = new H.service.Platform({
+      apikey: "2Oqh4QLTO1IjrSNM6NoWt2oiGhfxWG86_IZr_aTkaH4"
+    });
+
+    const defaultLayers = platform.createDefaultLayers();
+
+    // Create an instance of the map
+    const map = new H.Map(
+      this.mapRef.current,
+      defaultLayers.vector.normal.map,
+      {
+        // This map is centered over Europe
+        center: { lat: 12.2958, lng: 76.6394 },
+        zoom: 13.5,
+        pixelRatio: window.devicePixelRatio || 1
+      }
+    );
+
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    const ui = H.ui.UI.createDefault(map, defaultLayers);
+
+    this.setState({ map, platform });
+  }
+
+  componentWillUnmount() {
+    this.state.map.dispose();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div ref={this.mapRef} style={{ height: "100vh", width: "100vw" }} />
+        <UIComponent />
+      </div>
+    );
+
+  }
 }
 
 export default App;
